6361aa5452b7c286cb4a4042cd349ec96e1ca1ee
Vikranth3140 <vikranth22570@iiitd.ac.in>
2024-02-26 06:25:39 +0530

    Added more colours and made a pattern

--- pink flower/flower.py
+++ pink flower/flower.py
@@ -1,11 +1,20 @@
 import turtle
+
+colors = ["red", "blue", "green", "yellow", "orange", "purple", "pink", "white", "cyan", "magenta"]
+
 t = turtle.Turtle()
 s = turtle.Screen()
 s.bgcolor("black")
-t.pencolor("pink")
 t.speed(0)
+
 for i in range(150):
-    t.circle(190-i,90)
+    t.pencolor(colors[i % len(colors)])
+    t.fillcolor(colors[i % len(colors)])
+    t.begin_fill()
+    t.circle(190-i, 90)
     t.lt(90)
-    t.circle(190-i,90)
+    t.circle(190-i, 90)
     t.lt(18)
+    t.end_fill()
+
+turtle.done()
